ed97cf9d3457762e0e2789be831c2639a44992c1
Julien <42862794+makethpanda@users.noreply.github.com>
2021-11-07 23:49:19 +0100

    almost working prototype

--- Mini-projetJulien/chessMod.py
+++ Mini-projetJulien/chessMod.py
@@ -1,90 +1,312 @@
-import random
+from tkinter.constants import W
+
+
+class Piece:
+    def show(self):
+        return str(self.p)+str(self.color)
+    def __init__(self,color,piece,y,x):
+        self.color = color
+        self.p = piece
+        self.y = y
+        self.x = x
+    def move(self,x,y,check):
+        if (y,x) in check:
+            listplace(y,x,internalboard[self.y][self.x])
+            listplace(self.y,self.x,"__")
+            self.y = y
+            self.x = x
+        else:
+            print("invalid move")
+        return 
+    def showcolor(self):
+        return self.color
+class Rook(Piece):
+    def __init__(self, color, piece,y,x):
+        super().__init__(color, piece,y,x) 
+    def getmoverange(self):
+        moverange = []
+        x = self.x+1 
+        y = self.y
+        
+        while x<=7:
+            if board[y][x]!="__" and board[y][x][1]!=self.color:
+                
+                moverange.append((y,x))
+                break
+            elif board[y][x][1]==self.color:
+                break
+            else:
+                moverange.append((y,x))         
+            x+=1
+        x = self.x-1
+        while x>=0:
+            if board[y][x]!="__" and board[y][x][1]!=self.color:
+                moverange.append((y,x))
+                break
+            elif board[y][x][1]==self.color:
+                break
+            else:
+                moverange.append((y,x))         
+            x-=1
+        x = self.x
+        y = self.y+1
+        while y<=7:
+            if board[y][x]!="__" and board[y][x][1]!=self.color:
+                
+                moverange.append((y,x))
+                break
+            elif board[y][x][1]==self.color:
+                break
+            else:
+                moverange.append((y,x))         
+            y+=1
+        y = self.y-1
+        while y>=0:
+            if board[y][x]!="__" and board[y][x][1]!=self.color:
+                moverange.append((y,x))
+                break
+            elif board[y][x][1]==self.color:
+                break
+            else:
+                moverange.append((y,x))         
+            y-=1   
+        return moverange
+class Pawn(Piece):
+    def basepos(self):
+        if self.color=="b":
+            return 6
+        else:return 1
+    def __init__(self, color, piece,y,x):
+        super().__init__(color, piece,y,x) 
+        if self.color =="b":
+            self.advancement = -1
+        else:
+            self.advancement = 1
+    def getmoverange(self):
+        moverange = []
+        if isempty(board,self.y+self.advancement,self.x):#avancer de 1
+            moverange.append((self.y+self.advancement,self.x))
+        if self.y == self.basepos():
+            if isempty(board,self.y+2*self.advancement,self.x) and isempty(board,self.y+self.advancement,self.x):
+                moverange.append((self.y+2*self.advancement,self.x))
+        if self.x<7:
+            if not isempty(board,self.y+self.advancement,self.x+1):
+                if self.color != checkboard(board,self.y+self.advancement,self.x+1):
+                    moverange.append((self.y+self.advancement,self.x+1))
+        if 0<self.x:
+            if not isempty(board,self.y+self.advancement,self.x-1):
+                if self.color != checkboard(board,self.y+self.advancement,self.x-1):
+                    moverange.append((self.y+self.advancement,self.x-1))
+            return moverange
+    def geteatrange(self):
+        eatrange = []
+        if self.x<7:
+            if self.color != checkboard(board,self.y+self.advancement,self.x+1):
+                eatrange.append((self.y+self.advancement,self.x+1))
+        if 0<self.x:
+            if self.color != checkboard(board,self.y+self.advancement,self.x-1):
+                eatrange.append((self.y+self.advancement,self.x-1))
+        return eatrange
+class Knight(Piece):
+    def __init__(self, color, piece,y,x):
+        super().__init__(color, piece,y,x) 
+    def getmoverange(self):
+        moverange = []
+        y = self.y
+        x = self.x
+        movelist = []
+        if x-2>-1 and y+1<8:
+            movelist.append((x-2,y+1))
+        if x-2>-1 and y-1>-1:
+            movelist.append((x-2,y-1))
+        if x+2<8 and y+1<8:
+            movelist.append((x+2,y+1))
+        if x+2<8 and y-1>-1:
+            movelist.append((x+2,y-1))
+        if x-1>-1 and y+2<8:
+            movelist.append((x-1,y+2))
+        if x-1>-1 and y-2>-1:
+            movelist.append((x-1,y-2))
+        if x+1<8 and y+2<8:
+            movelist.append((x+1,y+2))
+        if x+1<8 and y-2>-1:
+            movelist.append((x+1,y-2))
+        for i in movelist:
+            if board[i[1]][i[0]]!="__" and board[i[1]][i[0]][1]!=self.color:
+                moverange.append((i[1],i[0]))
+            elif board[i[1]][i[0]][1]==self.color:
+                pass
+            else:
+                moverange.append((i[1],i[0]))
+        return moverange
+class Bishop(Piece):
+    def __init__(self, color, piece,y,x):
+        super().__init__(color, piece,y,x) 
+    def getmoverange(self):
+        moverange = []
+        x = self.x
+        y = self.y
+        #x+ y+ #WORKS I THINK
+        while x+1<8 and y+1<8:
+            x = x+1
+            y =y+1
+            if board[y][x]!="__" and board[y][x][1]!=self.color:
+                moverange.append((y,x))
+                break
+            elif board[y][x][1]==self.color:
+                break
+            else:
+                moverange.append((y,x))       
+        x = self.x
+        y = self.y
+        #x- y+ 
+        while x-1>-1 and y+1<8:
+            x = x-1
+            y =y+1
+            if board[y][x]!="__" and board[y][x][1]!=self.color:
+                moverange.append((y,x))
+                break
+            elif board[y][x][1]==self.color:
+                break
+            else:
+                moverange.append((y,x))      
+        x = self.x
+        y = self.y 
+        #x+ y-
+        while x+1<8 and y-1>-1:
+            x = x+1
+            y =y-1
+            if board[y][x]!="__" and board[y][x][1]!=self.color:
+                moverange.append((y,x))
+                break
+            elif board[y][x][1]==self.color:
+                break
+            else:
+                moverange.append((y,x))   
+        x = self.x
+        y = self.y       
+        #x- y-
+        while x-1>-1 and y-1>-1:
+            x = x-1
+            y =y-1
+            if board[y][x]!="__" and board[y][x][1]!=self.color:
+                moverange.append((y,x))
+                break
+            elif board[y][x][1]==self.color:
+                break
+            else:
+                moverange.append((y,x))         
+        return moverange
+class Queen(Piece):
+    def __init__(self, color, piece,y,x):
+        super().__init__(color, piece,y,x) 
+    def getmoverange(self):
+        moverange = []
+        x = self.x
+        y = self.y
+        moverange = Bishop(self.color,"B",y,x).getmoverange()+Rook(self.color,"R",y,x).getmoverange()
+        return moverange
+class King(Piece):
+    def __init__(self, color, piece,y,x):
+        super().__init__(color, piece,y,x) 
+    def getmoverange(self):
+        moverange = []
+        x = self.x
+        y = self.y
+        possiblemoves = []
+        if x-1>-1:
+            possiblemoves.append((y,x-1))
+            if y-1>-1:
+                possiblemoves.append((y-1,x-1))
+            if y+1<8:
+                possiblemoves.append((y+1,x-1))
+        if y-1>-1:
+            possiblemoves.append((y-1,x))
+        if x+1<8:
+            possiblemoves.append((y,x+1))
+            if y-1>-1:
+                possiblemoves.append((y-1,x+1))
+            if y+1<8:
+                possiblemoves.append((y+1,x+1))
+        if y+1<8:
+            possiblemoves.append((y+1,x))
+        for k in possiblemoves:
+            moverange.append(k)
+        for ligne in internalboard:
+                for p in ligne:
+                    if p != "__" and p.color != self.color and p.p != "p":
+                        for k in moverange:
+                            if k in p.getmoverange():
+                                moverange.remove(k)
+                    elif p != "__" and p.color != self.color and p.p =="p":
+                        for k in moverange:
+                            if k in p.geteatrange():
+                                print(k)
+                                moverange.remove(k)
+        print(moverange)
+        return moverange
+#setup vars---------------------------------------------------------
+internalboard =  [ ["__" for i in range(8)] for _ in range(8) ]
+board = [ ["__" for i in range(8)] for _ in range(8) ]
 length = ["a","b","c","d","e","f","g","h"]
 height = ["1","2","3","4","5","6","7","8"]
 tiles = [[length[i]+height[j]for i in range(8)] for j in range(8)]
-board = empty_lists = [ ["__" for i in range(8)] for _ in range(8) ]
-itemboard = empty_lists = [ ["__" for i in range(8)] for _ in range(8) ]
-def getpos(x):
-    return tiles.index(x)
-def showboard(b):
+movement = -1
+posb = 5
+def listplace(y,x,item):
+    internalboard[y][x]=item
+    if item != "__":
+        board[y][x]=item.show()
+    else:
+        board[y][x]="__"
+def isempty(board,y,x):
+    if board[y][x]=="__" and -1< x < 8 and -1< y < 8:
+        return True
+    else: 
+        return False
+def checkboard(board,y,x):
+    return board[y][x]
+def show(b):
     for i in b:
+        print()
         print(i)
-class Piece:
-    class Rook:
-        def __init__(self,color,pos):
-            self.color = color
-            if self.color == "B":#black
-                self.column = pos
-                self.advancement = 6
-            else:
-                self.column = pos
-                self.advancement = 1
-        def show(self):
-            return "P"
+def getboard():
+    return board
+for i in range(len(board[0])):
+    listplace(1,i,Pawn("w","p",1,i))
+    listplace(6,i,Pawn("b","p",6,i))
+listplace(0,0,Rook("w","R",0,0))
+listplace(0,7,Rook("w","R",0,7))
+listplace(7,0,Rook("b","R",7,0))
+listplace(7,7,Rook("b","R",7,7))
+listplace(0,1,Knight("w","N",0,1))
+listplace(0,6,Knight("w","N",0,6))
+listplace(7,6,Knight("b","N",7,6))
+listplace(7,1,Knight("b","N",7,1))
+listplace(0,2,Bishop("w","B",0,2))
+listplace(0,5,Bishop("w","B",0,5))
+listplace(7,2,Bishop("b","B",7,2))
+listplace(7,5,Bishop("b","B",7,5))
+listplace(0,4,Queen("w","Q",0,4))
+listplace(7,4,Queen("b","Q",7,4))
+listplace(7,3,King("b","K",7,3))
+listplace(0,3,King("w","K",0,3))
 
-    class Pawn:
-        def __init__(self,color,posx,posy):
-            self.color = color
-            if self.color == "B":#black
-                self.column = posx
-                if posy == -1:
-                    self.advancement = 6
-                else:
-                    self.advancement=posy
-            else:
-                self.column = posx
-                self.advancement = 1
-        def show(self):
-            if self.color =="B":
-                return "Pb"
-            else:
-                return"Pw"
-        def canmove(self,amount,board):
-            if self.color == "B":
-                if amount==2:
-                    assert self.advancement==2,"invalid move"
-                for i in range(amount):
-                    i = i+1
-                    assert board[self.advancement-i][self.column]=="_","no empty space"
-                self.advancement -= i
-                return True
-                
-        def caneat(self):
-            if  self.color == "B":
-                if self.column+1 > 7:
-                    return ([self.column-1],[self.advancement-1])
-                elif self.column-1 <0:
-                    return ([self.column+1],[self.advancement-1])
-                else:
-                    return ([self.column-1],[self.advancement-1]),([self.column+1],[self.advancement-1])
-            elif  self.color == "W":
-                if self.column+1 > 7:
-                    return ([self.column-1],[self.advancement+1])
-                elif self.column-1 <0:
-                    return ([self.column+1],[self.advancement+1])
-                else:
-                    return ([self.column-1],[self.advancement+1]),([self.column+1],[self.advancement+1])
-    def __init__(self, ptype, color, posx,posy=-1):
-        self.color = color
-        if ptype == "P":
-            self.piece = self.Pawn(color,posx,posy)
-    def __str__(self):
-            return self.piece
-    def move(self, toy, tox, board):
-        if board[toy][tox] 
-        board[toy][tox] = self.piece.show()
-        board[self.piece.advancement][self.piece.column] = "__"
-        self.piece.advancement = toy
-        self.piece.column = tox
-    def place(self, board):
-        board[self.piece.advancement][self.piece.column]=self.piece.show()
-
-            
-showboard(board)
-p1 = Piece("P","B",0)
-p2 = Piece("P","B",0,5)
-p1.place(board)
-p2.place(board)
-showboard(board)
-p1.move(5,0,board)
-showboard(board)
+#-----------------------------------------------------------------
+turn = 0
+def play(newmove):
+    global turn
+    if turn % 2 == 0:
+        colorturn = "w"
+    else:
+        colorturn = "b"
+    x = newmove
+    indexofthing = (length.index(x[0]),height.index(x[1]))
+    indexofmove = (length.index(x[2]),height.index(x[3]))
+    if internalboard[indexofthing[1]][indexofthing[0]].showcolor()  == colorturn:
+        mover = internalboard[indexofthing[1]][indexofthing[0]]
+        mover.move(indexofmove[0],indexofmove[1],mover.getmoverange())
+        show(board)
+        turn += 1
+    else:
+        None
--- Mini-projetJulien/tkinterchess.py
+++ Mini-projetJulien/tkinterchess.py
@@ -1,17 +1,19 @@
 import tkinter as tk
+import os 
+import chessmod
+dir_path = os.path.dirname(os.path.realpath(__file__))
 class GameBoard(tk.Frame):
     def __init__(self, parent, rows=8, columns=8, size=64, color1="white", color2="blue"):
         '''size is the size of a square, in pixels'''
-
         self.rows = rows
         self.columns = columns
         self.size = size
         self.color1 = color1
         self.color2 = color2
         self.pieces = {}
-
+    
         canvas_width = columns * size
-        canvas_height = rows * size
+        canvas_height = rows * size + 100
 
         tk.Frame.__init__(self, parent)
         self.canvas = tk.Canvas(self, borderwidth=0, highlightthickness=0,
@@ -54,11 +56,68 @@ class GameBoard(tk.Frame):
             self.placepiece(name, self.pieces[name][0], self.pieces[name][1])
         self.canvas.tag_raise("piece")
         self.canvas.tag_lower("square")
+        
 
 if __name__ == "__main__":
+    x = 0
     root = tk.Tk()
-    board = GameBoard(root)
-    board.pack(side="top", fill="both", expand="true", padx=4, pady=4)
-    pawn = tk.PhotoImage(file='pawn.png')
-    board.addpiece("player1", pawn, 1,0)
+    board = []
+    board.append(GameBoard(root))
+    board[x].pack(side="top", fill="both", expand="true", padx=4, pady=4)
+    bb = tk.PhotoImage(file='assets\Bb.png')
+    bh = tk.PhotoImage(file='assets\Bh.png')
+    bk = tk.PhotoImage(file='assets\Bk.png')
+    bp = tk.PhotoImage(file='assets\Bp.png')
+    bq = tk.PhotoImage(file='assets\Bq.png')
+    br = tk.PhotoImage(file='assets\Br.png')
+    wb = tk.PhotoImage(file='assets\Wb.png')
+    wh = tk.PhotoImage(file='assets\Wh.png')
+    wk = tk.PhotoImage(file='assets\Wk.png')
+    wp = tk.PhotoImage(file='assets\Wp.png')
+    wq = tk.PhotoImage(file='assets\Wq.png')
+    wr = tk.PhotoImage(file='assets\Wr.png')
+    def boardplace():
+        for i in reversed(range(8)):
+            for j in reversed(range(8)):
+                if chessmod.getboard()[i][j] == "Bb":
+                    board[x].addpiece("bbishop"+str(i+j), bb, i,j)
+                elif chessmod.getboard()[i][j] == "Nb":
+                    board[x].addpiece("bnight"+str(i+j), bh, i,j)
+                elif chessmod.getboard()[i][j] == "Kb":
+                    board[x].addpiece("bking"+str(i+j), bk, i,j)
+                elif chessmod.getboard()[i][j] == "pb":
+                    board[x].addpiece("bpawn"+str(i+j), bp, i,j)
+                elif chessmod.getboard()[i][j] == "Qb":
+                    board[x].addpiece("bqueen"+str(i+j), bq, i,j)
+                elif chessmod.getboard()[i][j] == "Rb":
+                    board[x].addpiece("brook"+str(i+j), br, i,j)
+                elif chessmod.getboard()[i][j] == "Bw":
+                    board[x].addpiece("wbishop"+str(i+j), wb, i,j)
+                elif chessmod.getboard()[i][j] == "Nw":
+                    board[x].addpiece("wnight"+str(i+j), wh, i,j)
+                elif chessmod.getboard()[i][j] == "Kw":
+                    board[x].addpiece("wking"+str(i+j), wk, i,j)
+                elif chessmod.getboard()[i][j] == "pw":
+                    board[x].addpiece("wpawn"+str(i+j), wp, i,j)
+                elif chessmod.getboard()[i][j] == "Qw":
+                    board[x].addpiece("wqueen"+str(i+j), wq, i,j)
+                elif chessmod.getboard()[i][j] == "Rw":
+                    board[x].addpiece("wrook"+str(i+j), wr, i,j)
+    boardplace()
+    def get(event):
+        global board
+        global x
+        x+= 1
+        chessmod.play(event.widget.get())
+        for widgets in board[x-1].winfo_children():
+            widgets.destroy()
+        board[x-1].destroy()
+        board.append(GameBoard(root))
+        board[x].pack(side="top", fill="both", expand="true", padx=4, pady=4)
+        boardplace()
+    root.title("chess")
+    e = tk.Entry(root, width=25)
+    e.bind('<Return>', get)
+    e.pack()
+
     root.mainloop()
